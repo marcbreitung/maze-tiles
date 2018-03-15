2100575e5e8bdc98cba1fb154678e5d0be63bd2f
Marc Breitung <info@marcbreitung.de>
2018-03-15 15:09:02 +0100

    added class Tiles

---
+++ src/Tile/Tiles.js
@@ -0,0 +1,7 @@
+export class Tiles {
+    constructor(parameters) {
+        this.width = parameters.width || 3;
+        this.height = parameters.height || 3;
+        this.tiles = [];
+    }
+}
---
+++ test/Unit/Tile/TilesTest.js
@@ -0,0 +1,21 @@
+let assert = require('chai').assert;
+
+import {Tile} from './../../../src/Tile/Tile';
+import {Tiles} from "../../../src/Tile/Tiles";
+
+suite('Tiles', function () {
+    suite('#constructor(parameters)', function () {
+        test('should set width, height and tiles with default values', function () {
+            let tiles = new Tiles({});
+            assert.propertyVal(tiles, 'width', 3);
+            assert.propertyVal(tiles, 'height', 3);
+            assert.deepPropertyVal(tiles, 'tiles', []);
+        });
+        test('should set width, height and tiles with parameter values', function () {
+            let tiles = new Tiles({'width': 5, 'height': 5});
+            assert.propertyVal(tiles, 'width', 5);
+            assert.propertyVal(tiles, 'height', 5);
+            assert.deepPropertyVal(tiles, 'tiles', []);
+        });
+    });
+});
