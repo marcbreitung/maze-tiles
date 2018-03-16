a2c2c3d61f2739bd688a7d9ea166a4855928e1be
Marc Breitung <info@marcbreitung.de>
2018-03-16 12:37:30 +0100

    moved hasConnection to class BitField

--- src/Tile/Tile.js
+++ src/Tile/Tile.js
@@ -39,49 +39,8 @@ export class Tile {
      */
     addNeighbour(row, column, tiles) {
         let id = `tile-${row}-${column}`;
-        if (`tile-${this.row}-${this.column}` !== id && tiles.tiles[id] && this.hasConnection(tiles.tiles[id])) {
+        if (`tile-${this.row}-${this.column}` !== id && tiles.tiles[id] && BitField.hasConnection(tiles.tiles[id], this)) {
             this.neighbours[id] = tiles.tiles[id];
         }
-    }
-
-    /**
-     * Checks if a connection exists between two tiles
-     * @param {Tile} tile - The tile to test
-     * @returns {boolean} - Returns true, if connection exists
-     */
-    hasConnection(tile) {
-        // is right
-        if (tile.row - this.row === 0 && tile.column - this.column === 1) {
-            let thisTileBit = this.walkable.filter((e, i) => (i + 1) % 3 === 0);
-            let tileTileBit = tile.walkable.filter((e, i) => (i + 1) % 3 === 1);
-            if (BitField.getBitField(...thisTileBit) & BitField.getBitField(...tileTileBit)) {
-                return true;
-            }
-        }
-        // is bottom
-        if (tile.row - this.row === 1 && tile.column - this.column === 0) {
-            let thisTileBit = this.walkable.slice(6, this.walkable.length);
-            let tileTileBit = tile.walkable.slice(0, 3);
-            if (BitField.getBitField(...thisTileBit) & BitField.getBitField(...tileTileBit)) {
-                return true;
-            }
-        }
-        // is left
-        if (tile.row - this.row === 0 && tile.column - this.column === -1) {
-            let thisTileBit = this.walkable.filter((e, i) => (i + 1) % 3 === 1);
-            let tileTileBit = tile.walkable.filter((e, i) => (i + 1) % 3 === 0);
-            if (BitField.getBitField(...thisTileBit) & BitField.getBitField(...tileTileBit)) {
-                return true;
-            }
-        }
-        // is top
-        if (tile.row - this.row === -1 && tile.column - this.column === 0) {
-            let thisTileBit = this.walkable.slice(0, 3);
-            let tileTileBit = tile.walkable.slice(6, this.walkable.length);
-            if (BitField.getBitField(...thisTileBit) & BitField.getBitField(...tileTileBit)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
--- test/Unit/BitField/BitFieldTest.js
+++ test/Unit/BitField/BitFieldTest.js
@@ -1,3 +1,5 @@
+import {Tile} from "../../../src/Tile/Tile";
+
 let assert = require('chai').assert;
 
 import {BitField} from "../../../src/BitField/BitField";
@@ -7,6 +9,53 @@ suite('getBitField', function () {
         test('should return arguments bit field', function () {
             assert.equal(7, BitField.getBitField(0, 0, 0, 0, 0, 0, 1, 1, 1));
             assert.equal(292, BitField.getBitField(1, 0, 0, 1, 0, 0, 1, 0, 0));
+        });
+    });
+    suite('#hasConnection(tile)', function () {
+        test('should return true if connection on the right exists', function () {
+            let tile_1_1 = new Tile({'row': 1, 'column': 1, 'walkable': [0, 1, 0, 1, 1, 1, 0, 1, 0]});
+            let tile_1_2 = new Tile({'row': 1, 'column': 2, 'walkable': [0, 0, 0, 1, 1, 1, 0, 0, 0]});
+            assert.isTrue(BitField.hasConnection(tile_1_2, tile_1_1));
+        });
+        test('should return false if no connection on the right exists', function () {
+            let tile_1_1 = new Tile({'row': 1, 'column': 1, 'walkable': [0, 1, 0, 1, 1, 1, 0, 1, 0]});
+            let tile_1_2 = new Tile({'row': 1, 'column': 2, 'walkable': [0, 0, 0, 0, 1, 1, 0, 0, 0]});
+            assert.isFalse(BitField.hasConnection(tile_1_2, tile_1_1));
+        });
+        test('should return true if connection on the bottom exists', function () {
+            let tile_1_1 = new Tile({'row': 1, 'column': 1, 'walkable': [0, 1, 0, 1, 1, 1, 0, 1, 0]});
+            let tile_2_1 = new Tile({'row': 2, 'column': 1, 'walkable': [0, 1, 0, 0, 1, 0, 0, 1, 0]});
+            assert.isTrue(BitField.hasConnection(tile_2_1, tile_1_1));
+        });
+        test('should return true if no connection on the bottom exists', function () {
+            let tile_1_1 = new Tile({'row': 1, 'column': 1, 'walkable': [0, 1, 0, 1, 1, 1, 0, 1, 0]});
+            let tile_2_1 = new Tile({'row': 2, 'column': 1, 'walkable': [0, 0, 0, 0, 1, 0, 0, 1, 0]});
+            assert.isFalse(BitField.hasConnection(tile_2_1, tile_1_1));
+        });
+        test('should return true if connection on the left exists', function () {
+            let tile_1_2 = new Tile({'row': 1, 'column': 2, 'walkable': [0, 0, 0, 1, 1, 1, 0, 0, 0]});
+            let tile_1_1 = new Tile({'row': 1, 'column': 1, 'walkable': [0, 1, 0, 1, 1, 1, 0, 1, 0]});
+            assert.isTrue(BitField.hasConnection(tile_1_1, tile_1_2));
+        });
+        test('should return true if no connection on the left exists', function () {
+            let tile_1_2 = new Tile({'row': 1, 'column': 2, 'walkable': [0, 0, 0, 1, 1, 1, 0, 0, 0]});
+            let tile_1_1 = new Tile({'row': 1, 'column': 1, 'walkable': [0, 1, 0, 0, 1, 0, 0, 1, 0]});
+            assert.isFalse(BitField.hasConnection(tile_1_1, tile_1_2));
+        });
+        test('should return true if connection on the top exists', function () {
+            let tile_2_1 = new Tile({'row': 2, 'column': 1, 'walkable': [0, 1, 0, 0, 1, 0, 0, 1, 0]});
+            let tile_1_1 = new Tile({'row': 1, 'column': 1, 'walkable': [0, 1, 0, 1, 1, 1, 0, 1, 0]});
+            assert.isTrue(BitField.hasConnection(tile_1_1, tile_2_1));
+        });
+        test('should return true if no connection on the top exists', function () {
+            let tile_2_1 = new Tile({'row': 2, 'column': 1, 'walkable': [0, 1, 0, 0, 1, 0, 0, 1, 0]});
+            let tile_1_1 = new Tile({'row': 1, 'column': 1, 'walkable': [0, 1, 0, 1, 1, 1, 0, 0, 0]});
+            assert.isFalse(BitField.hasConnection(tile_1_1, tile_2_1));
+        });
+        test('should return false tiles are not neighbours', function () {
+            let tile_1_1 = new Tile({'row': 1, 'column': 1, 'walkable': [0, 1, 0, 1, 1, 1, 0, 1, 0]});
+            let tile_1_3 = new Tile({'row': 1, 'column': 3, 'walkable': [0, 0, 0, 0, 1, 1, 0, 0, 0]});
+            assert.isFalse(BitField.hasConnection(tile_1_3, tile_1_1));
         });
     });
 });
