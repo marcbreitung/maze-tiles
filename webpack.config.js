b2ff5eba26f2260b426c928e96a30d66ee65022e
Marc Breitung <info@marcbreitung.de>
2019-03-05 21:39:17 +0100

    Updated node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,55 @@
 const path = require('path');
 
-module.exports = [{
-    entry: './src/maze-tiles.js',
-    output: {
-        filename: 'maze-tiles.min.js',
-        path: path.resolve(__dirname, 'dist'),
-        library: 'MazeTiles'
+module.exports = [
+    {
+        mode: 'production',
+        entry: './src/maze-tiles.js',
+        output: {
+            filename: 'maze-tiles.min.js',
+            path: path.resolve(__dirname, 'dist'),
+            library: 'MazeTiles'
+        },
+        module: {
+            rules: [
+                {
+                    enforce: "pre",
+                    test: /\.js$/,
+                    include: [path.resolve(__dirname, 'src')],
+                    exclude: [path.resolve(__dirname, 'node_modules')],
+                    loader: "eslint-loader",
+                },
+                {
+                    test: /\.js$/,
+                    include: [path.resolve(__dirname, 'src')],
+                    exclude: [path.resolve(__dirname, 'node_modules')],
+                    loader: "babel-loader",
+                },
+            ]
+        }
     },
-    module: {
-        rules: [
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                include: [path.resolve(__dirname, 'src')],
-                exclude: [path.resolve(__dirname, 'node_modules')],
-                loader: "eslint-loader",
-            },
-            {
-                test: /\.js$/,
-                include: [path.resolve(__dirname, 'src')],
-                exclude: [path.resolve(__dirname, 'node_modules')],
-                loader: "babel-loader",
-            },
-        ]
-    }
-}, {
-    entry: './src/maze-tiles.js',
-    output: {
-        filename: 'maze-tiles.node.min.js',
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: 'commonjs'
-    },
-    module: {
-        rules: [
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                include: [path.resolve(__dirname, 'src')],
-                exclude: [path.resolve(__dirname, 'node_modules')],
-                loader: "eslint-loader",
-            },
-            {
-                test: /\.js$/,
-                include: [path.resolve(__dirname, 'src')],
-                exclude: [path.resolve(__dirname, 'node_modules')],
-                loader: "babel-loader",
-            },
-        ]
-    }
-}];
+    {
+        mode: 'production',
+        entry: './src/maze-tiles.js',
+        output: {
+            filename: 'maze-tiles.node.min.js',
+            path: path.resolve(__dirname, 'dist'),
+            libraryTarget: 'commonjs'
+        },
+        module: {
+            rules: [
+                {
+                    enforce: "pre",
+                    test: /\.js$/,
+                    include: [path.resolve(__dirname, 'src')],
+                    exclude: [path.resolve(__dirname, 'node_modules')],
+                    loader: "eslint-loader",
+                },
+                {
+                    test: /\.js$/,
+                    include: [path.resolve(__dirname, 'src')],
+                    exclude: [path.resolve(__dirname, 'node_modules')],
+                    loader: "babel-loader",
+                },
+            ]
+        }
+    }];
